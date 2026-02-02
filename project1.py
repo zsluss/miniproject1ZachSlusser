b5588f25488bdf808b54c93e512a35acf962e84e
zsluss <zluss@gmail.com>
2026-02-02 09:25:25 -0600

    add gitignore

--- project1.py
+++ project1.py
@@ -1,6 +1,30 @@
+# Zach Slusser
+# Advanced Python - Project 1
+# Due: 02/15/2026
+
 import yfinance as yf
-gm = yf.Ticker("GM")
-print(gm.info['open'])
-print(gm.info['previousClose'])
-print(gm.info['dayHigh'])
-print(gm.info['dayLow'])
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+allStocks = np.array([])
+
+#list of the 5 stocks to get data for
+stocklist = ["GM", "TSLA", "TM","F","RACE"]
+getDates = True
+for stock in stocklist:
+    ticker = yf.Ticker(stock)
+    #dates were in timestamp format and i wanted it cleaner
+    if getDates:
+        hist = ticker.history(period="1mo").tail(10)
+        date = np.array(hist.index)
+        datesonly = np.array(date.astype('datetime64[D]'))
+
+        getDates = False
+    hist = ticker.history(period="1mo").tail(10)
+    allStocks = np.append(allStocks, hist['Close'].to_numpy())
+
+
+print(datesonly)
+print(allStocks)
+plt.plot([allStocks], [datesonly])
