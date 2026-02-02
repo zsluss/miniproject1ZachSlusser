cb868ed29085f66de0defee246aa730907d47c84
zsluss <zluss@gmail.com>
2026-02-02 10:12:24 -0600

    refactor stock data processing and enhance chart titles; add requirements.txt for dependencies

--- project1.py
+++ project1.py
@@ -16,15 +16,15 @@ stocklist = ["GM", "TSLA", "TM","F","RACE"]
 getDates = True
 for stock in stocklist:
     ticker = yf.Ticker(stock)
-    #dates were in timestamp format and i wanted it cleaner
     hist = ticker.history(period="1mo").tail(10)
+    #dates were in timestamp format and i wanted it cleaner
     if getDates:
-        hist = ticker.history(period="1mo").tail(10)
         date = np.array(hist.index)
         datesonly = np.array(date.astype('datetime64[D]'))
         getDates = False
     stockdata[stock] = []
     for price in hist['Close']:
+        # i decided to round it thinking it would show on the chart but it didnt really matter, just makes the printing cleaner
         price = round(price, 2)
         stockdata[stock].append(price)
     
@@ -33,7 +33,8 @@ for stock in stocklist:
     plt.xlabel('Date')
     plt.xticks(rotation=45,fontsize=6)
     plt.ylabel('Closing Price USD')
-    plt.title('Closing Stock Prices Over Last 10 Days')
+    plt.title('Closing Stock Prices for ' + stock + ' Over Last 10 Closing Days (' + str(datesonly[0]) + ' to ' + str(datesonly[-1]) + ')')
+    # not sure if i should do a legend for a single stock and I put it on the top but I did it
     plt.legend()
     plt.savefig('./charts/' + stock + '_prices.png') 
     plt.show()
